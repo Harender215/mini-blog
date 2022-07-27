bc895b33c1540b23cd5874481280951d0664ed3a
Harender singh chauhan <harendersingh7896@gmail.com>
2022-07-27 12:11:32 +0530

    second commit

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fuahjt^-qzw^oo%@2p$+&(%!%x(jh-)hmf2k!+c$$d63hsa2r='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["harender123.herokuapp.com","localhost"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'Whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
